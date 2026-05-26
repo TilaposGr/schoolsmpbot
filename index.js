04c1e0e5cf8dfd021cd16459c7d79599de587a20
TilaposGr <tilemaxaki@gmail.com>
2026-05-26 22:35:34 +0300

    Remove web server and bot initialization code
    Removed web server setup and bot creation logic.

--- index.js
+++ index.js
@@ -39,30 +39,3 @@ function createBot() {
 }
 
 createBot();
-const mineflayer = require('mineflayer');
-const express = require('express');
-const app = express();
-
-// Web server layer required by Render's platform
-app.get('/', (req, res) => res.send('Bot is active.'));
-app.listen(process.env.PORT || 3000);
-
-function createBot() {
-  const bot = mineflayer.createBot({
-    host: '127.0.0.1', 
-    port: 19836,           
-    username: 'the guy she tells you not to worry about',    
-    version: '1.21.1'       
-  });
-
-  bot.on('spawn', () => {
-    console.log('Bot logged into Minecraft server.');
-    setInterval(() => {
-      bot.setControlState('jump', true);
-      setTimeout(() => bot.setControlState('jump', false), 500);
-    }, 30000);
-  });
-
-  bot.on('end', () => setTimeout(createBot, 5000));
-}
-createBot();
