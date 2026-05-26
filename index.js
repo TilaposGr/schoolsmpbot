a0dcef96d519605b072a650d3970ed013f110263
TilaposGr <tilemaxaki@gmail.com>
2026-05-26 22:39:41 +0300

    Update Minecraft bot connection details

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function createBot() {
   console.log('Attemping connection to Minecraft server...');
   
   const bot = mineflayer.createBot({
-    host: 'YOUR_SERVER_IP', // Ensure NO port is in this line (e.g. '142.44.22.10')
-    port: 25565,            // Ensure this is your numerical 5-digit port
-    username: 'AFK_Bot_247',    
-    version: '1.20.1'       // Verify your exact base Minecraft version
+    host: 'schoolsmpseason3a1.play.hosting', // Ensure NO port is in this line (e.g. '142.44.22.10')
+    port: 22404,            // Ensure this is your numerical 5-digit port
+    username: 'tgstuntwa',    
+    version: '1.21.1'       // Verify your exact base Minecraft version
   });
 
   bot.on('inject_allowed', () => {
